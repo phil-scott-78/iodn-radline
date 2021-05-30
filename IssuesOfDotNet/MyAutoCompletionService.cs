3eade627d145ee72315ef01a95e5420fb7c4ef1d
Patrik Svensson <patriksvensson@users.noreply.github.com>
2021-05-31 01:50:26 +0200

    Fix autocomplete (somewhat) (#1)

---
+++ IssuesOfDotNet/MyAutoCompletionService.cs
@@ -0,0 +1,41 @@
+﻿using RadLine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Encodings.Web;
+
+namespace IssuesOfDotNet
+{
+    public sealed class MyAutoCompletionService
+    {
+        private readonly HttpClient _client;
+
+        public MyAutoCompletionService(HttpClient client)
+        {
+            _client = client ?? throw new System.ArgumentNullException(nameof(client));
+        }
+
+        public AutoCompleteResults? GetCompletions(LineBuffer buffer)
+        {
+            var encoded = UrlEncoder.Default.Encode(buffer.Content);
+            var pos = buffer.CursorPosition - 1;
+            var url = @$"https://issuesof.net/api/completion?q={encoded}&pos={pos}";
+            var result = _client.GetFromJsonAsync<AutoCompleteResults>(url).Result;
+
+            if (result?.List == null)
+            {
+                return null;
+            }
+
+            return result;
+        }
+    }
+
+    public sealed class AutoCompleteResult
+    {
+        public List<string> List { get; set; }
+        public int From { get; set; }
+        public int To { get; set; }
+    }
+}
---
+++ IssuesOfDotNet/MyServiceProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+
+namespace IssuesOfDotNet
+{
+    public sealed class MyServiceProvider : IServiceProvider
+    {
+        private readonly MyAutoCompletionService _service;
+
+        public MyServiceProvider(HttpClient client)
+        {
+            _service = new MyAutoCompletionService(client);
+        }
+
+        public object? GetService(Type serviceType)
+        {
+            if (serviceType == typeof(MyAutoCompletionService))
+            {
+                return _service;
+            }
+
+            return null;
+        }
+    }
+}
